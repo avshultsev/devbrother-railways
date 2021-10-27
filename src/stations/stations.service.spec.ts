b21ed8e5cded782d3571351d55fe25053ed24be1
Alexey <avshultsev@gmail.com>
2021-10-27 11:20:10 +0300

    StationService testing start

---
+++ src/stations/stations.service.spec.ts
@@ -0,0 +1,31 @@
+describe('The StationsService', () => {
+  // getAllStations
+  describe('when getting all stations', () => {
+    // TODO
+  });
+
+  // getStationsWithFilter
+  describe('when getting stations with filter', () => {
+    // TODO
+  });
+
+  // getStationById
+  describe('when getting station by id', () => {
+    // TODO
+  });
+
+  // getStationByName
+  describe('when getting station by name', () => {
+    // TODO
+  });
+
+  // createStation
+  describe('when creating a station', () => {
+    // TODO
+  });
+
+  // deleteStation
+  describe('when deleting a station', () => {
+    // TODO
+  });
+});
