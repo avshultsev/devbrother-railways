04031b10bd78e36edc78fc9cc63be058c2fb615c
Alexey <avshultsev@gmail.com>
2021-10-27 18:16:31 +0300

    Routes Service testing finished

--- src/routes/routes.service.spec.ts
+++ src/routes/routes.service.spec.ts
@@ -1,6 +1,9 @@
 import { Test } from '@nestjs/testing';
 import { RouteDetailsService } from '../route-details/route-details.service';
+import { RouteDetail } from '../route-details/routeDetails.entity';
+import { Station } from '../stations/stations.entity';
 import { StationsService } from '../stations/stations.service';
+import { CreateRouteDto } from './dto/create-route.dto';
 import { Route } from './routes.entity';
 import { RoutesRepository } from './routes.repository';
 import { RoutesService } from './routes.service';
@@ -23,6 +26,8 @@ describe('The Routes Service', () => {
   let getRoutesForSingleStation: jest.Mock;
   let getRouteDetailsByRoute: jest.Mock;
   let route: Route;
+  let station_A: Station;
+  let station_B: Station;
 
   beforeEach(async () => {
     getStationByName = jest.fn();
@@ -54,10 +59,12 @@ describe('The Routes Service', () => {
 
     routesService = module.get(RoutesService);
     mockRepository = module.get(RoutesRepository);
+    station_A = { id: '2', title: 'A' };
+    station_B = { id: '3', title: 'B' };
     route = {
       id: '1',
-      arrivalPoint: { id: '2', title: 'A' },
-      departurePoint: { id: '3', title: 'B' },
+      arrivalPoint: station_A,
+      departurePoint: station_B,
       travelTime: 1,
     };
   });
@@ -75,26 +82,135 @@ describe('The Routes Service', () => {
 
   // getRoutesByStations
   describe('when getting routes by stations', () => {
-    // TODO
+    beforeEach(() => {
+      getStationByName.mockResolvedValue({ id: '1' });
+    });
+
+    describe('and when routes were found', () => {
+      beforeEach(() => {
+        mockRepository.find.mockResolvedValue([{ id: 'routeByEdgeStations' }]);
+        mockRepository.findByMixedStations.mockResolvedValue([
+          {
+            id: 'routeByMixedStations',
+          },
+        ]);
+        getRoutesByTwoWayStations.mockResolvedValue(['routeByTwoWayStations']);
+      });
+      it('should return an array of routes', async () => {
+        const routes = await routesService.getRoutesByStations('1', '2');
+        expect(routes).toHaveLength(3);
+      });
+    });
+
+    describe('and when routes were not found', () => {
+      beforeEach(() => {
+        mockRepository.find.mockResolvedValue([]);
+        mockRepository.findByMixedStations.mockResolvedValue([]);
+        getRoutesByTwoWayStations.mockResolvedValue([]);
+      });
+      it('should throw an error', async () => {
+        await expect(
+          routesService.getRoutesByStations('A', 'B'),
+        ).rejects.toThrow();
+      });
+    });
   });
 
   // getRoutesPassingThroughStation
-  describe('when getting routes passing through th station', () => {
-    // TODO
+  describe('when getting routes passing through the station', () => {
+    beforeEach(() => {
+      getRoutesForSingleStation.mockResolvedValue([{ route: { id: '1' } }]);
+      mockRepository.findByStation.mockReturnValue([{ id: '2' }]);
+    });
+    it('should return an array of route ids', async () => {
+      const routes = await routesService.getRoutesPassingThroughStation('abc');
+      expect(routes).toHaveLength(2);
+    });
   });
 
   // getFullRouteInfo
   describe('when getting full route info', () => {
-    // TODO
+    let routeDetail_1: RouteDetail;
+    let routeDetail_2: RouteDetail;
+
+    beforeEach(() => {
+      routeDetail_1 = {
+        id: '100',
+        stationOrder: 1,
+        route,
+        timeOffset: 1,
+        wayStation: { id: '2', title: 'Title' },
+      };
+      routeDetail_2 = { ...routeDetail_1, stationOrder: 2 };
+      mockRepository.findOne.mockResolvedValue(route);
+      getRouteDetailsByRoute.mockResolvedValue([routeDetail_1, routeDetail_2]);
+    });
+    it('should return route info and sorted route details', async () => {
+      const { routeDetails, routeInfo } = await routesService.getFullRouteInfo(
+        route,
+      );
+      expect(routeInfo).toBe(route);
+      expect(routeDetails).toHaveLength(2);
+      expect(routeDetails[0]).toHaveProperty('stationOrder', 1);
+      expect(routeDetails[1]).toHaveProperty('stationOrder', 2);
+    });
   });
 
   // createRoute
   describe('when creating a route', () => {
-    // TODO
+    const createRouteData: CreateRouteDto = {
+      arrivalPoint: 'a',
+      departurePoint: 'b',
+      travelTime: 1,
+    };
+
+    describe('and when both endpoints exist', () => {
+      beforeEach(() => {
+        getStationByName.mockResolvedValue(station_A);
+        mockRepository.create.mockResolvedValue(route);
+        mockRepository.save.mockResolvedValue(() => null);
+      });
+      it('should return a new route', async () => {
+        const newRoute = await routesService.createRoute(createRouteData);
+        expect(newRoute).toBe(route);
+      });
+    });
+
+    describe('and one of the endpoints does not exist', () => {
+      beforeEach(() => {
+        getStationByName
+          .mockResolvedValueOnce(station_A)
+          .mockResolvedValueOnce(null);
+      });
+      it('should throw an error', async () => {
+        await expect(
+          routesService.createRoute(createRouteData),
+        ).rejects.toThrow();
+      });
+    });
   });
 
   // deleteRoute
   describe('when deleting a route', () => {
-    // TODO
+    const fn: (affected: number) => jest.Mock = (affected) =>
+      mockRepository.delete.mockReturnValue({ affected });
+    describe('and the route was found', () => {
+      beforeEach(() => {
+        fn(1);
+      });
+      it('should return undefined', async () => {
+        const result = await routesService.deleteRoute('1');
+        expect(result).toBe(undefined);
+      });
+    });
+
+    describe('and the route was not found', () => {
+      beforeEach(() => {
+        fn(0);
+      });
+      it('should throw an error', async () => {
+        await expect(routesService.deleteRoute('1')).rejects.toThrow();
+      });
+    });
   });
 });
